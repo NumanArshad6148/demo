34d5dbcd1f719fe1a09ece94524b03a513a857e3
numan <numn@mailinator.com>
2024-09-30 16:54:05 +0500

    changes from local repo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Todo from './Todo';
 function App() {
 
   const[count, setCount] = useState<number>(0);
+  const[data, setData] = useState(undefined);
+
 
   const increment = (): void => {
      setCount(count + 1);
